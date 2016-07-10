a71e9e8387954a8ab2fe728ece70ff0f4ae333ca
Michael Manning <netitrinr@gmail.com>
2016-07-09 19:33:12 -0600

    Added comments

--- app/Post.php
+++ app/Post.php
@@ -16,55 +16,115 @@ class Post extends Model
         restore as baseRestore;
     }
 
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
     protected $dates = ['deleted_at'];
 
-    protected $fillable = ['body', 'syntax', 'user_id'];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'body', 'syntax', 'user_id'
+    ];
 
+    /**
+     * The number of models to return for pagination.
+     *
+     * @var int
+     */
     protected $perPage = 20;
 
+    /**
+     * The mappings for the SyntaxType enum to foatmat methods
+     *
+     * @var array
+     */
     protected $formats = [
         SyntaxType::BBCode => 'formatBBCode',
         SyntaxType::Markdown => 'formatMarkdown'
     ];
 
+    /**
+     * Many Posts are created by Users
+     */
     public function user(){
         return $this->belongsTo(User::class);
     }
 
+    /**
+     * Many Posts belong to a Thread
+     */
     public function thread(){
         return $this->belongsTo(Thread::class);
     }
 
+    /**
+     * A Post may have a parent Post
+     */
     public function parent(){
         return $this->belongsTo(Post::class, 'parent_id');
     }
 
+    /**
+     * A Post may have child Posts
+     */
     public function children(){
         return $this->hasMany(Post::class, 'parent_id');
     }
 
+    /**
+     * Override to update who deleted a Post
+     */
     public function delete(){
         $this->update(['deleted_by', Auth::user()->id]);
         return parent::delete();
     }
 
+    /**
+     * Override to reset who deleted a Post
+     */
     public function restore(){
         $this->update(['deleted_by', NULL]);
         return $this->baseRestore();
     }
 
+    /**
+     * The html content of the Post
+     *
+     * @var string
+     */
     public function getHtmlAttribute() :string{
         return trim(call_user_func([$this, $this->formats[$this->syntax]]));
     }
 
+    /**
+     * Converts the Post to Html from BBCode format
+     *
+     * @return string
+     */
     protected function formatBBCode() :string{
         return '<p>'.BBCode::parse($this->body).'</p>';
     }
 
+    /**
+     * Converts the Post to Html from Markdown format
+     *
+     * @return string
+     */
     protected function formatMarkdown() :string{
         return Markdown::convertToHtml($this->body);
     }
 
+    /**
+     * If the Post is a child or not
+     *
+     * @return bool
+     */
     public function getIsChildAttribute(){
         return $this->parent_id != NULL;
     }
--- app/Thread.php
+++ app/Thread.php
@@ -21,44 +21,88 @@ class Thread extends Model
      */
     protected $dates = ['deleted_at', 'locked_at', 'blocked_at'];
 
-    protected $fillable = ['name', 'link', 'nsfw', 'user_id'];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'link', 'nsfw', 'user_id'
+    ];
 
+    /**
+     * The number of models to return for pagination.
+     *
+     * @var int
+     */
     protected $perPage = 20;
 
+    /**
+     * The table likes are stored in
+     *
+     * @var string
+     */
     private static $likes_table = 'thread_likes';
 
+    /**
+     * Many Threads are created by Users
+     */
     public function user(){
         return $this->belongsTo(User::class);
     }
 
+    /**
+     * Many Posts are created by Users
+     */
     public function posts(){
         return $this->hasMany(Post::class);
     }
 
+    /**
+     * Posts that are replies to the Thread
+     */
     public function rootPosts(){
         return $this->posts()->doesntHave('parent');
     }
 
+    /**
+     * Posts that are replies to Posts
+     */
     public function childPosts(){
         return $this->posts()->has('parent');
     }
 
+    /**
+     * A Thread can have one Poll
+     */
     public function poll(){
         return $this->hasOne(Poll::class);
     }
 
+    /**
+     * Many Threads can have many Tags
+     */
     public function tags(){
         return $this->belongsToMany(Tag::class);
     }
 
+    /**
+     * A Thread can be liked by the Auth'd User
+     */
     public function like(){
         return DB::table(self::$likes_table)->insert(['thread_id' => $this->id, 'user_id' => Auth::user()->id]);
     }
 
+    /**
+     * A Thread can be unliked by the Auth'd User
+     */
     public function unlike(){
         return DB::table(self::$likes_table)->where('thread_id', $this->id)->where('user_id', Auth::user()->id)->delete();
     }
 
+    /**
+     * Helper method to toggle Liking a Thread
+     */
     public function toggleLike(){
         if(DB::table(self::$likes_table)->where('thread_id', $this->id)->where('user_id', Auth::user()->id)->count() > 0){
             return $this->unlike();
@@ -67,32 +111,57 @@ class Thread extends Model
         }
     }
 
+    /**
+     * A Thread can be pinned
+     */
     public function pin(){
         return $this->update(['pinned_at'=>Carbon::now()]);
     }
 
+    /**
+     * A Thread can be unpinned
+     */
     public function unpin(){
         return $this->update(['pinned_at'=>NULL]);
     }
 
+    /**
+     * Helper method to toggle Pinning a Thread
+     */
     public function togglePin(){
         return $this->pinned ? $this->unpin() : $this->pin();
     }
 
+    /**
+     * Override to update who deleted a Thread
+     */
     public function delete(){
         $this->update(['deleted_by' => Auth::user()->id]);
         return parent::delete();
     }
 
+    /**
+     * Override to reset who deleted a Thread
+     */
     public function restore(){
         $this->update(['deleted_by' => NULL]);
         return $this->baseRestore();
     }
 
+    /**
+     * The total likes a Thread has
+     *
+     * @return int
+     */
     public function getLikesAttribute(){
         return DB::table($likes_table)->where('thread_id', $this->id)->count();
     }
 
+    /**
+     * If a Thread is pinned or not
+     *
+     * @return bool
+     */
     public function getPinnedAttribute(){
         return $this->pinned_at != NULL;
     }
