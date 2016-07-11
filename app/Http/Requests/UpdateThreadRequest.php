ecc676905883f01bfc79080b27badfa565a85112
Michael Manning <netitrinr@gmail.com>
2016-07-10 18:23:32 -0600

    Updated requests

--- app/Http/Requests/UpdateThreadRequest.php
+++ app/Http/Requests/UpdateThreadRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use App\Http\Requests\Request;
 
-class UpdatePostRequest extends Request
+class StoreThreadRequest extends Request
 {
     /**
      * Get the validation rules that apply to the request.
@@ -14,6 +14,9 @@ class UpdatePostRequest extends Request
     public function rules()
     {
         return [
+            'name' => 'required|max:100'
+            'link' => 'url'
+            'nsfw' = 'required|boolean'
             'body' => 'required|max:300',
             'syntax' => 'required|enum:SyntaxType'
         ];
