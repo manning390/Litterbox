bf0106419899abf9094645aebcfb7c07f63fc9c2
Michael Manning <michaelmanning390@gmail.com>
2016-07-22 15:43:35 -0600

    Fixed Ip Logging

--- app/Ip.php
+++ app/Ip.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ip extends Model
 {
+    protected $fillable = ['ip'];
+
     public function users(){
         return $this->belongsToMany(User::class)->withTimestamps();
     }
